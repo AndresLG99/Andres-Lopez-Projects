4ddae0e80fecd5d80a2b973f86922ed9a21372bc
Andres Lopez <ing.andlopgam5@gmail.com>
2025-06-26 10:32:15 -0700

    added new project, updated README file and set up instructions for new project

---
+++ guessing_number_game.py
@@ -0,0 +1,17 @@
+# Ask for user's name
+# Gives an intro to the game
+# Starts with 8 attempts
+# Range between 1 and 100
+# Select a random number in the range
+# Asks user to input a number in the range
+# Handles numbers out of range, doesn't affect remaining attempts
+# If the inputed number is lower than the chosen number
+    # Update range
+    # Print message stating the inputed number is lower
+# If the inputed number is higher than the chosen number
+    # Update range
+    # Print message stating the inputed number is higher
+# If the inputed number is correct
+    # Print winning message
+    # Attempts taken
+# Loop until user wins or attempts are equal to 0
